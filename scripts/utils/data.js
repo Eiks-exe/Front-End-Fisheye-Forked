9ecc181f3f7fa69a34ccb644a8b060d4f6e0dbab
Eiks <eiks_art@outlook.com>
2022-10-24 10:41:05 -0400

    DataFetching&DataInitialization

---
+++ scripts/utils/data.js
@@ -0,0 +1,16 @@
+export const globalState = {
+    photographers: [],
+    media: []
+}
+
+export const initData =async () => {
+    const response  = await fetch("../../data/photographers.json")
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+
+    const data = await response.json();
+    globalState.media = data.media;
+    globalState.photographers = data.photographers;
+    console.log(data)
+}
